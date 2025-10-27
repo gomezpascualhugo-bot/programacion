85f3a7304494bb91e691122d357007b663866deb
recisbinto-354 <gomezpascualhugo@gmail.com>
2025-10-27 16:45:36 +0100

    Delete doWhile directory

--- doWhile/src/doWhile.java
+++
@@ -1,9 +0,0 @@
-public class doWhile {
-    static void main(String[] args) {
-        int i = 5;
-        do{
-            System.out.println(i);
-            i ++;
-        }while (i<0);
-    }
-}
